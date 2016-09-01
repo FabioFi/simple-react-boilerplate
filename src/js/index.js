7cd97f4b585f0b4d640a6a891dce73eaf449141f
Abraham Estrada <abraham.estrada@gmail.com>
2016-08-31 21:30:29 -0600

    Fix #3 Remove `history`
    It is not necessary, only to declare custom location.

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, useRouterHistory, IndexRoute } from 'react-router'
-import { createHistory } from 'history'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 import App from './App'
 import Home from './Home'
 import Hello from './Hello'
 
 import 'css/style'
-
-const browserHistory = useRouterHistory(createHistory)({
-  basename: '/'
-})
 
 render((
   <Router history={browserHistory}>
